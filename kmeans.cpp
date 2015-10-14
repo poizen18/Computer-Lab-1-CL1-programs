250b946f0f720418075d46057d496c2ddace6b07
Achilles G <achillesgaikwad@gmail.com>
2015-10-14 22:54:35 +0530

    update to change clusterhead
    changing the cluster head to one of the aforementioned data objects in the program.
    so basically, the object who is closest to the ACTUAL centroid becomes the centroid.

--- kmeans.cpp
+++ kmeans.cpp
@@ -3,12 +3,13 @@
 // Author      : poizen
 // Version     :
 // Copyright   : opensource for life
-// Description : kmeans
+// Description : Hello World in C++, Ansi-style
 //============================================================================
 
 #include <iostream>
 #include<cstdlib>
 #include<math.h>
+#include<algorithm>
 using namespace std;\
 
 class Point{
@@ -54,12 +55,12 @@ float distance(float x1,float y1,float x2, float y2){
 
 int main() {
 	bool nochange[2]={false,false};
-
+	float dist1=0,dist2=0;
 	Point data[10], centroid[2], oldcentroid[2];
 //initializing dataset to random points
 	for(int i=0;i<10;i++){
-		float xx=rand()%40;
-		float yy=rand()%40;
+		float xx=rand()%10;
+		float yy=rand()%10;
 		data[i].setx(xx);
 		data[i].sety(yy);
 	}
@@ -71,8 +72,8 @@ int main() {
 	}
 //init centr
 	for(int i=0;i<2;i++){
-				float xx=rand()%40;
-				float yy=rand()%40;
+				float xx=rand()%10;
+				float yy=rand()%10;
 				centroid[i].setx(xx);
 				centroid[i].sety(yy);
 	}
@@ -85,8 +86,8 @@ int main() {
 //calculate distance of each point towards the centroid and assign which dataset belongs to which cluster
 	for(int i=0;i<10;i++){
 
-		float dist1=distance(data[i].x,data[i].y,centroid[0].x,centroid[0].y);
-		float dist2=distance(data[i].x,data[i].y,centroid[1].x,centroid[1].y);
+		 dist1=distance(data[i].x,data[i].y,centroid[0].x,centroid[0].y);
+		 dist2=distance(data[i].x,data[i].y,centroid[1].x,centroid[1].y);
 
 		if(dist1>=dist2){
 
@@ -113,25 +114,70 @@ int main() {
 					oldcentroid[i].sety(centroid[i].gety());
 					}
 	for(int i=0;i<10;i++){
-
+		float closex[2],closey[2];
+		int min[2]={999,999};
 		if(data[i].getclusterid()==0){
 			x=x+data[i].getx();
 			c1++;
 			y=y+data[i].gety();
-
+			dist1=distance(data[i].x,data[i].y,centroid[0].x,centroid[0].y);
+			/*if(dist1<min1){
+				min1=dist1;
+				closex[0]=data[i].getx();
+				closey[0]=data[i].gety();
+			}*/
 		}
 		else{
 			x2=x2+data[i].getx();
 			c2++;
 			y2=y2+data[i].gety();
+			/*dist1=distance(data[i].x,data[i].y,centroid[0].x,centroid[0].y);*/
 
 		}
 		//i declared new variables rather than the ones mentioned already because it gave me some error, and im too lazy to correct that :)
 		float x3=x/c1,y3=y/c1,y4=y2/c2,x4=x2/c2;
+
 		centroid[0].setx(x3);
 		centroid[0].sety(y3);
 		centroid[1].setx(x4);
 		centroid[1].sety(y4);
+//what you do hereafter is check which of the objects is having closest distance to the centroid, and you make that object
+		//the new centroil :)
+		for(int i=0;i<10;i++){
+
+			if(data[i].clusterid==0){
+				 dist1=distance(data[i].x,data[i].y,centroid[0].x,centroid[0].y);
+				 if(dist1<min[0]){
+				 				min[0]=dist1;
+				 				closex[0]=data[i].getx();
+				 				closey[0]=data[i].gety();
+
+				 			}
+
+			}
+			else{
+				 dist2=distance(data[i].x,data[i].y,centroid[1].x,centroid[1].y);
+				 if(dist2<min[1]){
+				 				 				min[1]=dist2;
+				 				 				closex[1]=data[i].getx();
+				 				 				closey[1]=data[i].gety();
+
+				 				 			}
+			}
+
+
+
+			}
+		// cout<<"data object closest to the centroid 1 is made the new centroid"<<closex[0]<<" " <<closey[0];
+		 //cout<<"data object closest to the centroid 2 is made the new centroid"<<closex[1]<<" " <<closey[1];
+		 	 	centroid[0].setx(closex[0]);
+		 		centroid[0].sety(closey[0]);
+		 		centroid[1].setx(closex[1]);
+		 		centroid[1].sety(closey[1]);
+
+
+
+
 	}
 	for(int i=0;i<2;i++){
 
@@ -140,7 +186,7 @@ int main() {
 			}
 
 				cout<<endl;
-				cout<<"for CENTROID "<<i<<" X: "<<centroid[i].getx()<<" Y: "<<centroid[i].gety()<<endl;
+				//cout<<"for CENTROID "<<i<<" X: "<<centroid[i].getx()<<" Y: "<<centroid[i].gety()<<endl;
 			}
 			}
 
